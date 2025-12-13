09e32f1a01ca337181e5d6d309d15b7ef810dc7d
Tsedniya <ktsedniya@gmail.com>
2025-12-13 10:45:17 +0300

    backend:post_test

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,14 +4,14 @@ const getContacts = (req,res) =>{
 }
 
 const createContact = (req,res)=>{
-    console.log("The request body is:", req.body);
+    console.log('the requst body is here', req.body)
     res.status(200).json({message:"create all contacts"});
 };
 
 const getContact = (req,res)=>{
     res.status(200).json({message:`Get contacts for ${req.params.id}`});
 }
-//updated type
+
 const updateContact= (req,res)=>{
     res.status(200).json({message:`update contacts for ${req.params.id}`});
 }
