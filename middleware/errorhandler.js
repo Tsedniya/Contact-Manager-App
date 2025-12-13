3cfb633245c19849a1a81a24733c71f17cae5e8f
Tsedniya <ktsedniya@gmail.com>
2025-12-13 11:49:17 +0300

    backend:errorhandler

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,9 +1,39 @@
+const {constants} = require('../constants');
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
-  res.status(statusCode).json({
-    message: err.message,
-    stackTrace: err.stack
-  });
+  switch (statusCode){
+    case  VALIDATION_ERROR:
+      res.json({
+         title:"Validation Failed",
+         message: err.message,
+         stackTrace: err.stack
+      });
+      break;
+    case  NOT_FOUND:
+        res.json({
+        title:"Not Found",
+        message: err.message,
+        stackTrace: err.stack
+       });
+     case  FORBIDDEN:
+        res.json({
+        title:" FORBIDDEN",
+        message: err.message,
+        stackTrace: err.stack
+       });
+     case  UNAUTHORIZED:
+        res.json({
+        title:" UNAUTHORIZED",
+        message: err.message,
+        stackTrace: err.stack
+       });
+      
+    
+    default:
+      break;
+  }
+  
+
 };
 
 module.exports = errorHandler;
