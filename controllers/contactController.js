1b811f9508ef94d72652dc6a9b04e8d24e6891df
Tsedniya <ktsedniya@gmail.com>
2025-12-13 10:20:17 +0300

    backend:contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,13 +3,8 @@ const getContacts = (req,res) =>{
     res.status(200).json({message:"get all contact"})
 }
 
-const  createContact = (req,res)=>{
+const createContact = (req,res)=>{
     console.log("The request body is:", req.body);
-    const {name,email,phone} = req.body;
-    if(!name || !email  || !phone){
-        res.status(400);
-        throw new Error("All fields are mandatory")
-    }
     res.status(200).json({message:"create all contacts"});
 };
 
