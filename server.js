bb43f414f7d9cc0b148d465a682d8fef014b0d3f
Tsedniya <ktsedniya@gmail.com>
2026-01-24 14:28:08 +0300

    backend:dbconnection

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorhandler");
 const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection"); // <-- import your connectDb function here
+
 const app = express();
+
+// Connect to MongoDB
+connectDb();
+
 const port = process.env.PORT || 5000;
 
-
 app.use(express.json());
-app.use("/api/contacts",require("./routes/contactRoutes")
-);
+app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use(errorHandler);
 
-app.listen(port,()=>{
-    console.log(`server running on port ${port}`)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
