4bd2656091c39af118cb13b99295fb6df71521fa
Tsedniya <ktsedniya@gmail.com>
2026-01-27 20:51:08 +0300

    backend:validateToken

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 
@@ -31,6 +32,26 @@ const registerUser = asyncHandler(async(req,res)=>{
 })
 
 const loginUser = asyncHandler(async(req,res)=>{
+    const {email,password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const user = await User.findOne({email});
+    if(user &&(await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            username:user.username,
+            email:user.email,
+            id:user.id,
+        },
+        process.env.ACCESS_TOKEN_SECERT,
+        {expiresIn:"1m"}
+    );
+    res.status(200).json({accessToken});
+    }else{
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
     res.json({message:"Login the user"});
 })
 
---
+++ middleware/validTokenHandler.js
@@ -0,0 +1,6 @@
+const asyncHandler= require("express-async-handler");
+const jwt = require("jsonwebtoken");
+
+const validToken = asyncHandler(async(req,res,next)=>{
+    
+})
